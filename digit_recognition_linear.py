efe21f4f38413d5f4a11efe685631c5664d7bcc7
Divya <ds2299@cornell.edu>
2019-11-04 13:33:54 -0500

    Changed name of Linear Neural Net file

--- digit_recognition_linear.py
+++ digit_recognition_linear.py
@@ -2,7 +2,7 @@ import torch
 from torchvision import datasets, transforms
 from torch import nn, optim
 from torch.utils import data
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 
 
 class BasicNN(nn.Module):
@@ -43,7 +43,7 @@ class BasicNN(nn.Module):
                 _, prediction = torch.max(output.data, 1)
                 test_results += (prediction == labels).sum().item()
                 total_labels += labels.size(0)
-        self.accuracy = test_results/total_labels
+        self.accuracy = test_results / total_labels
 
 
 if __name__ == '__main__':
@@ -65,21 +65,21 @@ if __name__ == '__main__':
     hidden_layers = [256, 128, 64]
     output_length = 10
 
-    temp = iter(mnist_trainset)
-    images, labels = next(temp)
-    plt.imshow(images[0].numpy().squeeze(), cmap='gray_r')
-    plt.show()
-    # neuralnet = nn.Sequential(nn.Linear(input_length, hidden_layers[0]),
-    #                           nn.ReLU(), nn.Linear(hidden_layers[0], hidden_layers[1]),
-    #                           nn.ReLU(), nn.Linear(hidden_layers[1], hidden_layers[2]),
-    #                           nn.ReLU(), nn.Linear(hidden_layers[2], output_length),
-    #                           nn.LogSoftmax(dim=1))
+    # temp = iter(mnist_trainset)
+    # images, labels = next(temp)
+    # plt.imshow(images[0].numpy().squeeze(), cmap='gray_r')
+    # plt.show()
+    neuralnet = nn.Sequential(nn.Linear(input_length, hidden_layers[0]),
+                              nn.ReLU(), nn.Linear(hidden_layers[0], hidden_layers[1]),
+                              nn.ReLU(), nn.Linear(hidden_layers[1], hidden_layers[2]),
+                              nn.ReLU(), nn.Linear(hidden_layers[2], output_length),
+                              nn.LogSoftmax(dim=1))
 
-    # loss_fxn = nn.NLLLoss()
-    # optimizer = optim.SGD(neuralnet.parameters(), lr=0.01)
+    loss_fxn = nn.NLLLoss()
+    optimizer = optim.SGD(neuralnet.parameters(), lr=0.01)
 
-    # my_training = BasicNN(optimizer, loss_fxn, neuralnet, trainset_generator)
-    # my_training.training_pass(35)
-    # my_training.validation_pass(valset_generator)
-    # print(my_training.train_loss / len(trainset_generator))
-    # print(100*my_training.accuracy)
+    my_training = BasicNN(optimizer, loss_fxn, neuralnet, trainset_generator)
+    my_training.training_pass(35)
+    my_training.validation_pass(valset_generator)
+    print(my_training.train_loss / len(trainset_generator))
+    print(100 * my_training.accuracy)
